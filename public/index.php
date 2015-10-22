319dfc960f28ecaa7778613a1a0dd50c4714618b
Wanderson <wandersonwhcr@gmail.com>
2015-10-22 15:48:02 -0200

    Issue #3 Instalação do Zend Framework utilizando o Composer.

--- public/index.php
+++ public/index.php
@@ -1,1 +1,15 @@
-<?php phpinfo();
+<?php
+
+chdir(dirname(__DIR__));
+
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+
+if (!is_file('vendor/autoload.php')) {
+    exit(-1);
+}
+
+require 'vendor/autoload.php';
+
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();
