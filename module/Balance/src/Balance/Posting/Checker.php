924b0cfb31c30958f2c2e69ff1daa9441927e3e4
Wanderson <wandersonwhcr@gmail.com>
2015-10-23 11:22:04 -0200

    Issue #7 Documentação de código.

--- module/Balance/src/Balance/Posting/Checker.php
+++ module/Balance/src/Balance/Posting/Checker.php
@@ -4,13 +4,27 @@ namespace Balance\Posting;
 
 use InvalidArgumentException;
 
+/**
+ * Verificador de Lançamentos Balanceados
+ */
 class Checker
 {
     const CREDIT = 'CREDIT';
     const DEBIT  = 'DEBIT';
 
+    /**
+     * Diferença Encontrada
+     * @var float
+     */
     protected $difference = 0.0;
 
+    /**
+     * Adiciona um Valor ao Verificador
+     *
+     * @param  string  $type  Tipo de Valor (Crédito ou Débito)
+     * @param  float   $value Valor para Adição
+     * @return Checker Próprio Objeto para Encadeamento
+     */
     public function addValue($type, $value)
     {
         // Pesquisa
@@ -30,11 +44,24 @@ class Checker
         return $this;
     }
 
+    /**
+     * Verificador Válido?
+     *
+     * Efetua a verificação se todos os valores adicionados estão balanceados entre crédito e débito, retornando verdade
+     * se a diferença encontrada é igual a zero.
+     *
+     * @return bool Confirmação Solicitada
+     */
     public function isValid()
     {
         return $this->difference === 0.0;
     }
 
+    /**
+     * Apresenta a Diferença Encontrada
+     *
+     * @return float Valor Solicitado
+     */
     public function getDifference()
     {
         return $this->difference;
