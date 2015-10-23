58bf1fa420090417187ab69ed345e03105a2f05d
Peterson WM <petersonwpm@gmail.com>
2015-10-23 17:36:29 -0200

    Issue #6 Criação da tabela de contas e do tipo de operação

---
+++ module/Balance/migrations/20151023185903_create_table_financial_accounts.php
@@ -0,0 +1,36 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateTableFinancialAccounts extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $this->table('financial_accounts')
+            ->addColumn('name', 'varchar')
+            ->addColumn('type', 'financial_operation')
+            ->addColumn('description', 'varchar')
+            ->create();
+    }
+}
