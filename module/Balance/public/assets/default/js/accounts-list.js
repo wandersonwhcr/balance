84579d855a1a348cabd961a8493e5d9e80ca3ae7
Wanderson <wandersonwhcr@gmail.com>
2015-11-09 15:22:57 -0200

    Issue #51 Configuração do elemento para ordenação.

--- module/Balance/public/assets/default/js/accounts-list.js
+++ module/Balance/public/assets/default/js/accounts-list.js
@@ -2,13 +2,30 @@ $(function () {
     // Movimentar Linhas
     (function () {
         // Tabela
-        var table = $('.table');
+        var table   = $('.table');
+        var columns = table.find('thead tr:first-child th');
         // Draggable
         table.find('tbody').sortable({
             axis: 'y',
             scroll: true,
             handle: '.table-move',
-            cancel: ''
+            cancel: '',
+            placeholder: 'ui-state-highlight',
+            helper: function (event, ui) {
+                // Configurar Colunas
+                ui.find('td').each(function (i) {
+                    // Coluna da Linha
+                    var column = $(this);
+                    // Configurar Largura do Helper
+                    column.width(columns.eq(i).width());
+                });
+                // Largura de Colunas
+                return ui;
+            },
+            start: function (event, ui) {
+                // Altura do Placeholder
+                ui.placeholder.height(ui.item.height());
+            }
         }).disableSelection();
     })();
 });
