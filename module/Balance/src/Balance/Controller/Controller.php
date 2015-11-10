306944d9e98cdf1f8c5f8b1c5acf19e47c40f117
Wanderson <wandersonwhcr@gmail.com>
2015-11-10 17:42:07 -0200

    Issue #77 Trait para ação de editar elementos.

--- module/Balance/src/Balance/Controller/Controller.php
+++ module/Balance/src/Balance/Controller/Controller.php
@@ -16,6 +16,7 @@ class Controller extends AbstractActionController
 {
     // Traits
     use IndexActionTrait;
+    use EditActionTrait;
     use RemoveActionTrait;
 
     /**
@@ -85,59 +86,5 @@ class Controller extends AbstractActionController
     public function getRedirectRouteName()
     {
         return $this->redirectRouteName;
-    }
-
-    /**
-     * Editar Elemento
-     *
-     * @return ViewModel Modelo de Visualização
-     */
-    public function editAction()
-    {
-        // Camada de Modelo
-        $model = $this->getModel();
-        // Chave Primária
-        $params = $this->params()->fromRoute();
-        // Remover Controladora e Ação
-        $params = array_diff_key($params, array_flip(array('controller', 'action')));
-        // Dados Enviados?
-        if ($this->getRequest()->isPost()) {
-            // Captura de Dados
-            $data = $this->getRequest()->getPost();
-            // Tratamento
-            try {
-                // Salvar Dados
-                $model->save($data);
-                // Sucesso
-                $this->flashMessenger()->addSuccessMessage('Os dados foram salvos com sucesso.');
-                // Redirecionamento
-                return $this->redirect()->toRoute($this->getRedirectRouteName());
-            } catch (ModelException $e) {
-                // Erro Encontrado
-                $this->flashMessenger()->addWarningMessage('Verifique o preenchimento dos campos em destaque.');
-            }
-        } else {
-            // Chave Primária?
-            if ($params) {
-                // Tratamento
-                try {
-                    // Carregar Elemento
-                    $model->load(new Parameters($params));
-                } catch (ModelException $e) {
-                    // Erro Encontrado
-                    $this->flashMessenger()->addErrorMessage('Impossível carregar os dados solicitados.');
-                    // Redirecionamento
-                    return $this->redirect()->toRoute($this->getRedirectRouteName());
-                }
-            }
-        }
-        // Utilizar Template do Roteamento
-        $this->getServiceLocator()->get('ViewManager')
-            ->getInjectTemplateListener()->setPreferRouteMatchController(true);
-        // Visualização
-        return new ViewModel(array(
-            'type' => ($params ? 'edit' : 'add'),
-            'form' => $model->getForm(),
-        ));
     }
 }
---
+++ module/Balance/src/Balance/Controller/EditActionTrait.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Balance\Controller;
+
+use Balance\Model\ModelException;
+use Zend\Stdlib\Parameters;
+use Zend\View\Model\ViewModel;
+
+/**
+ * Trait para Edição de Elementos
+ */
+trait EditActionTrait
+{
+    /**
+     * Editar Elemento
+     *
+     * @return ViewModel Modelo de Visualização
+     */
+    public function editAction()
+    {
+        // Camada de Modelo
+        $model = $this->getModel();
+        // Chave Primária
+        $params = $this->params()->fromRoute();
+        // Remover Controladora e Ação
+        $params = array_diff_key($params, array_flip(array('controller', 'action')));
+        // Dados Enviados?
+        if ($this->getRequest()->isPost()) {
+            // Captura de Dados
+            $data = $this->getRequest()->getPost();
+            // Tratamento
+            try {
+                // Salvar Dados
+                $model->save($data);
+                // Sucesso
+                $this->flashMessenger()->addSuccessMessage('Os dados foram salvos com sucesso.');
+                // Redirecionamento
+                return $this->redirect()->toRoute($this->getRedirectRouteName());
+            } catch (ModelException $e) {
+                // Erro Encontrado
+                $this->flashMessenger()->addWarningMessage('Verifique o preenchimento dos campos em destaque.');
+            }
+        } else {
+            // Chave Primária?
+            if ($params) {
+                // Tratamento
+                try {
+                    // Carregar Elemento
+                    $model->load(new Parameters($params));
+                } catch (ModelException $e) {
+                    // Erro Encontrado
+                    $this->flashMessenger()->addErrorMessage('Impossível carregar os dados solicitados.');
+                    // Redirecionamento
+                    return $this->redirect()->toRoute($this->getRedirectRouteName());
+                }
+            }
+        }
+        // Utilizar Template do Roteamento
+        $this->getServiceLocator()->get('ViewManager')
+            ->getInjectTemplateListener()->setPreferRouteMatchController(true);
+        // Visualização
+        return new ViewModel(array(
+            'type' => ($params ? 'edit' : 'add'),
+            'form' => $model->getForm(),
+        ));
+    }
+}
