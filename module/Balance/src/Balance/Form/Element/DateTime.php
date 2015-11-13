ccfff5608d473deb4dbffc431efa49d1141b449e
Wanderson <wandersonwhcr@gmail.com>
2015-11-13 14:45:21 -0200

    Issue #63 Correção em tempo de contrutor: se opções eram informadas em construtor de formulário, a inicialização era sobrescrita. Portanto, a inicialização do elemento é feita antes da configuração informada no construtor.

--- module/Balance/src/Balance/Form/Element/DateTime.php
+++ module/Balance/src/Balance/Form/Element/DateTime.php
@@ -9,10 +9,43 @@ use Zend\Form\Element\DateTime as ZendDateTime;
  */
 class DateTime extends ZendDateTime
 {
-    public function init()
+    /**
+     * {@inheritdoc}
+     */
+    public function getAttribute($key)
     {
-        $this
-            ->setAttribute('class', 'form-control-datetimepicker')
-            ->setOption('add-on-append', '<span class="glyphicon glyphicon-calendar"></span>');
+        // Captura
+        $value = parent::getAttribute($key);
+        // Parâmetro
+        if ($key === 'class') {
+            // Capturar Valor
+            $classes = array_filter(array_map('trim', explode(' ', $value)));
+            // Adicionar Classe
+            array_unshift($classes, 'form-control-datetimepicker');
+            // Aplicação
+            $value = implode(' ', $classes);
+            // Configuração
+            $this->setAttribute($key, $value);
+        }
+        // Apresentação
+        return $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOption($option)
+    {
+        // Captura
+        $value = parent::getOption($option);
+        // Parâmetro?
+        if ($option === 'add-on-append' && !$value) {
+            // Valor Padrão
+            $value = '<span class="glyphicon glyphicon-calendar"></span>';
+            // Configuração
+            $this->setOption($option, $value);
+        }
+        // Apresentação
+        return $value;
     }
 }
