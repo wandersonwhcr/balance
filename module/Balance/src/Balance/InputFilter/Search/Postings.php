34a69dad7fe27bc16c901a1a415a0085e72d1181
Wanderson <wandersonwhcr@gmail.com>
2015-11-27 10:00:52 -0200

    Issue #56 Incluir validações em campos de pesquisa.

--- module/Balance/src/Balance/InputFilter/Search/Postings.php
+++ module/Balance/src/Balance/InputFilter/Search/Postings.php
@@ -2,11 +2,43 @@
 
 namespace Balance\InputFilter\Search;
 
+use Zend\Filter;
+use Zend\InputFilter\Input;
 use Zend\InputFilter\InputFilter;
+use Zend\Validator;
 
 /**
  * Filtro de Pesquisa para Contas
  */
 class Postings extends InputFilter
 {
+    public function init()
+    {
+        // Palavras Chave
+        $input = (new Input('keywords'))
+            ->setRequired(false);
+        $this->add($input);
+
+        // Contas
+        $input = (new Input('account_id'))
+            ->setRequired(false);
+        $input->getFilterChain()
+            ->attach(new Filter\ToInt())
+            ->attach(new Filter\ToNull());
+        $this->add($input);
+
+        // Data e Hora Inicial
+        $input = (new Input('datetime_begin'))
+            ->setRequired(false);
+        $input->getValidatorChain()
+            ->attach(new Validator\Date(array('format' => 'd/m/Y H:i:s')));
+        $this->add($input);
+
+        // Data e Hora Final
+        $input = (new Input('datetime_end'))
+            ->setRequired(false);
+        $input->getValidatorChain()
+            ->attach(new Validator\Date(array('format' => 'd/m/Y H:i:s')));
+        $this->add($input);
+    }
 }
