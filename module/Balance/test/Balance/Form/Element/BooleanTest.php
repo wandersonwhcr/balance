8101e36fa159bb121f519d4ced20000afeb61f55
Wanderson <wandersonwhcr@gmail.com>
2015-11-30 12:13:49 -0200

    Issue #88 Testes para elemento de formulário do tipo booleano.

---
+++ module/Balance/test/Balance/Form/Element/BooleanTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Balance\Form\Element;
+
+use Balance\Model\BooleanType;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class BooleanTest extends TestCase
+{
+    public function testValueOptions()
+    {
+        $element = new Boolean();
+
+        $values = $element->getValueOptions();
+
+        $this->assertCount(2, $values);
+        $this->assertEquals(array(
+            BooleanType::YES => 'Sim',
+            BooleanType::NO  => 'Não',
+        ), $values);
+    }
+}
