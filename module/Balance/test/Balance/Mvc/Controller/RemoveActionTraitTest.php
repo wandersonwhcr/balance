27be7ad51cf5dbf8954c7f706da1c31b090a74e0
Wanderson <wandersonwhcr@gmail.com>
2015-12-08 16:39:03 -0200

    Issue #88 Testes unitários em remoção de elementos em controladora.

--- module/Balance/test/Balance/Mvc/Controller/RemoveActionTraitTest.php
+++ module/Balance/test/Balance/Mvc/Controller/RemoveActionTraitTest.php
@@ -7,11 +7,40 @@ use PHPUnit_Framework_TestCase as TestCase;
 use Zend\Http\PhpEnvironment\Request;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\Mvc\Router\RouteMatch;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
 use Zend\ServiceManager\ServiceManager;
 
 class RemoveActionController
     extends AbstractActionController
     implements ModelAwareInterface, RedirectRouteNameAwareInterface
+{
+    use ModelAwareTrait;
+    use RedirectRouteNameAwareTrait;
+    use RemoveActionTrait;
+}
+
+class RemoveActionWithoutControllerController
+    implements ModelAwareInterface, RedirectRouteNameAwareInterface, ServiceLocatorAwareInterface
+{
+    use ModelAwareTrait;
+    use RedirectRouteNameAwareTrait;
+    use RemoveActionTrait;
+    use ServiceLocatorAwareTrait;
+}
+
+class RemoveActionWithoutModelController
+    extends AbstractActionController
+    implements RedirectRouteNameAwareInterface
+{
+    use ModelAwareTrait;
+    use RedirectRouteNameAwareTrait;
+    use RemoveActionTrait;
+}
+
+class RemoveActionWithoutRedirectRouteNameController
+    extends AbstractActionController
+    implements ModelAwareInterface
 {
     use ModelAwareTrait;
     use RedirectRouteNameAwareTrait;
@@ -51,6 +80,15 @@ class RemoveActionTraitTest extends TestCase
             case 'remove-action-controller':
                 $controller = new RemoveActionController();
                 break;
+            case 'remove-action-without-controller-controller':
+                $controller = new RemoveActionWithoutControllerController();
+                break;
+            case 'remove-action-without-model-controller':
+                $controller = new RemoveActionWithoutModelController();
+                break;
+            case 'remove-action-without-redirect-route-name-controller':
+                $controller = new RemoveActionWithoutRedirectRouteNameController();
+                break;
         }
 
         // Configurações
@@ -58,15 +96,22 @@ class RemoveActionTraitTest extends TestCase
             ->setModel($model)
             ->setServiceLocator($serviceLocator);
 
-        // Plugin de Redirecionamento
-        $redirect = $this->getMock('Zend\Mvc\Controller\Plugin\Redirect');
-        // Configurações
-        $controller->getPluginManager()->setService('redirect', $redirect);
+        // Tratamentos
+        switch ($type) {
+            case 'remove-action-controller':
+            case 'remove-action-without-model-controller':
+            case 'remove-action-without-redirect-route-name-controller':
+                // Plugin de Redirecionamento
+                $redirect = $this->getMock('Zend\Mvc\Controller\Plugin\Redirect');
+                // Configurações
+                $controller->getPluginManager()->setService('redirect', $redirect);
 
-        // Configurar Parâmetros de Despacho
-        $controller->getEvent()->setRouteMatch(new RouteMatch(array(
-            'action' => 'remove',
-        )));
+                // Configurar Parâmetros de Despacho
+                $controller->getEvent()->setRouteMatch(new RouteMatch(array(
+                    'action' => 'remove',
+                )));
+                break;
+        }
 
         // Apresentação
         return $controller;
@@ -90,6 +135,42 @@ class RemoveActionTraitTest extends TestCase
         $model = $controller->getModel()
             ->method('remove')
             ->will($this->throwException(new ModelException('Invalid Element')));
+
+        // Execução
+        $controller->dispatch(new Request());
+    }
+
+    public function testRemoveActionWithoutController()
+    {
+        // Verificação
+        $this->setExpectedException('Exception', 'Invalid Controller');
+
+        // Inicialização
+        $controller = $this->getController('remove-action-without-controller-controller');
+
+        // Execução
+        $controller->removeAction();
+    }
+
+    public function testRemoveActionWithoutModel()
+    {
+        // Verificação
+        $this->setExpectedException('Exception', 'Invalid Controller');
+
+        // Inicialização
+        $controller = $this->getController('remove-action-without-model-controller');
+
+        // Execução
+        $controller->dispatch(new Request());
+    }
+
+    public function testRemoveActionWithoutRedirectRouteName()
+    {
+        // Verificação
+        $this->setExpectedException('Exception', 'Invalid Controller');
+
+        // Inicialização
+        $controller = $this->getController('remove-action-without-redirect-route-name-controller');
 
         // Execução
         $controller->dispatch(new Request());
