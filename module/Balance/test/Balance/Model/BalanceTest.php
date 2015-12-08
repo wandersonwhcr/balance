442b3a03ba81d79b4ad4a4f82a80667427f5de83
Wanderson <wandersonwhcr@gmail.com>
2015-12-08 10:44:56 -0200

    Issue #88 Início dos testes em pesquisa de balancete.

--- module/Balance/test/Balance/Model/BalanceTest.php
+++ module/Balance/test/Balance/Model/BalanceTest.php
@@ -7,6 +7,7 @@ use Zend\Form\Form;
 use Zend\Form\FormElementManager;
 use Zend\InputFilter\InputFilterPluginManager;
 use Zend\ServiceManager\ServiceManager;
+use Zend\Stdlib\Parameters;
 
 class BalanceTest extends TestCase
 {
@@ -15,7 +16,6 @@ class BalanceTest extends TestCase
         $model = new Balance();
 
         $formElementManager = new FormElementManager();
-
         $inputFilterManager = new InputFilterPluginManager();
 
         $serviceLocator = new ServiceManager();
@@ -28,5 +28,44 @@ class BalanceTest extends TestCase
         $formSearch = $model->getFormSearch();
         $this->assertInstanceOf('Balance\Form\Search\Balance', $formSearch);
         $this->assertSame($formSearch, $model->getFormSearch());
+    }
+
+    public function testFetch()
+    {
+        $model = new Balance();
+
+        $formElementManager = new FormElementManager();
+        $inputFilterManager = new InputFilterPluginManager();
+
+        $serviceLocator = new ServiceManager();
+        $serviceLocator
+            ->setService('FormElementManager', $formElementManager)
+            ->setService('InputFilterManager', $inputFilterManager);
+
+        $model->setServiceLocator($serviceLocator);
+
+        $data = array(
+            array(
+                'id'   => 1,
+                'name' => 'one',
+            ),
+            array(
+                'id'   => 2,
+                'name' => 'two',
+            ),
+        );
+
+        $persistence = $this->getMock('Balance\Model\Persistence\PersistenceInterface');
+        $persistence
+            ->expects($this->atLeastOnce())
+            ->method('fetch')
+            ->will($this->returnValue($data));
+        $serviceLocator->setService('Balance\Model\Persistence\Balance', $persistence);
+
+        $result = $model->fetch(new Parameters(array(
+            'datetime' => '10/10/2010 10:10:10',
+        )));
+
+        $this->assertEquals($data, $result);
     }
 }
