42114099909eb30b2aacad2a985b43f6f35a72a1
Wanderson <wandersonwhcr@gmail.com>
2015-12-09 15:38:56 -0200

    Issue #88 Remoção da classe de centralização do aplicativo do Code Coverage.

--- phpunit.php
+++ phpunit.php
@@ -1,9 +1,49 @@
 <?php
 
-chdir(__DIR__);
+namespace Balance\Test\Mvc
+{
+    use Zend\Mvc\Application as MvcApplication;
 
-require 'vendor/autoload.php';
+    /**
+     * Aplicativo MVC para Testes
+     */
+    final class Application
+    {
+        /**
+         * Instância de Aplicativo para Acesso
+         * @type MvcApplication
+         */
+        private static $application;
 
-$application = Zend\Mvc\Application::init(require 'config/application.config.php');
+        /**
+         * Configuração de Instância de Aplicativo
+         *
+         * @param MvcApplication $application Instância para Configuração
+         */
+        public static function setApplication(MvcApplication $application)
+        {
+            self::$application = $application;
+        }
 
-Balance\Test\Mvc\Application::setApplication($application);
+        /**
+         * Apresentação de Instância de Aplicativo
+         *
+         * @return MvcApplication Instância Configurada
+         */
+        public static function getApplication()
+        {
+            return self::$application;
+        }
+    }
+}
+
+namespace
+{
+    chdir(__DIR__);
+
+    require 'vendor/autoload.php';
+
+    $application = Zend\Mvc\Application::init(require 'config/application.config.php');
+
+    Balance\Test\Mvc\Application::setApplication($application);
+}
