687753997240fb9e3be755fbf787946f3ca9836f
Wanderson <wandersonwhcr@gmail.com>
2015-12-10 09:24:53 -0200

    Issue #107 Remoção de regra conflitante no PHP 5.6 e PHP 7.

--- phpcs.php
+++ phpcs.php
@@ -3,5 +3,10 @@ $finder = Symfony\CS\Finder\DefaultFinder::create()
     ->in('module/Balance/src');
 return Symfony\CS\Config\Config::create()
     ->level(Symfony\CS\FixerInterface::CONTRIB_LEVEL)
-    ->fixers(array('-short_array_syntax', '-align_double_arrow', '-logical_not_operators_with_spaces'))
+    ->fixers(array(
+        '-short_array_syntax',
+        '-align_double_arrow',
+        '-logical_not_operators_with_spaces',
+        '-no_blank_lines_before_namespace',
+    ))
     ->finder($finder);
