ec8c56e2b05de845673bbca9100e7a185a61edc7
Wanderson <wandersonwhcr@gmail.com>
2015-12-11 21:07:55 -0200

    Issue #151 Testes unitários que representam o problema da tarefa.

---
+++ module/Balance/test/Balance/Bugs/Issue151Test.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Balance\Bugs;
+
+use Balance\Model\AccountType;
+use Balance\Model\BooleanType;
+use Balance\Model\EntryType;
+use Balance\Model\ModelException;
+use Balance\Mvc\Application;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Stdlib\Parameters;
+
+class Issue151Test extends TestCase
+{
+    public function testSimpleSavePosting()
+    {
+        // Localizador de Serviços
+        $serviceLocator = Application::getApplication()->getServiceManager();
+
+        // Camada de Modelo
+        $mAccounts = $serviceLocator->get('Balance\Model\Persistence\Accounts');
+
+        // Conta A
+        $accountA = new Parameters(array(
+            'type'        => AccountType::ACTIVE,
+            'name'        => 'Issue 151 A',
+            'description' => 'Account A',
+            'accumulate'  => BooleanType::NO,
+        ));
+        // Salvar
+        $mAccounts->save($accountA);
+
+        // Conta B
+        $accountB = new Parameters(array(
+            'type'        => AccountType::ACTIVE,
+            'name'        => 'Issue 151 B',
+            'description' => 'Account B',
+            'accumulate'  => BooleanType::NO,
+        ));
+        // Salvar
+        $mAccounts->save($accountB);
+
+        // Camada de Modelo
+        $mPostings = $serviceLocator->get('Balance\Model\Postings');
+
+        // Lançamento
+        $posting = new Parameters(array(
+            'id'          => '',
+            'datetime'    => '10/10/2010 10:10:10',
+            'description' => 'Issue 151 Posting A',
+            'entries'     => array(
+                array(
+                    'account_id' => (string) $accountA['id'],
+                    'type'       => EntryType::CREDIT,
+                    'value'      => '100,00',
+                ),
+                array(
+                    'account_id' => (string) $accountB['id'],
+                    'type'       => EntryType::DEBIT,
+                    'value'      => '100,00',
+                ),
+            ),
+        ));
+
+        try {
+            // Salvar
+            $mPostings->save($posting);
+        } catch (ModelException $e) {
+            // Capturar Formulário
+            $form = $mPostings->getForm();
+            // Formulário Válido?
+            if ($form->isValid()) {
+                // Erro em outro lugar!
+                throw $e;
+            }
+            // Capturar Mensagens do Formulário
+            $messages = $form->getMessages();
+            // Erro Encontrado
+            $this->fail('Invalid Form ' . json_encode($messages));
+        }
+    }
+}
