0776d58e7440469825c0f65ff409745e7b54c05f
Wanderson <wandersonwhcr@gmail.com>
2015-12-11 15:16:58 -0200

    Issue #104 Criação de método para captura de formatador de números.

--- module/Balance/src/Balance/I18n/I18n.php
+++ module/Balance/src/Balance/I18n/I18n.php
@@ -2,6 +2,8 @@
 
 namespace Balance\I18n;
 
+use NumberFormatter;
+
 /**
  */
 class I18n
@@ -43,5 +45,17 @@ class I18n
     public function getLocale()
     {
         return $this->locale;
+    }
+
+    /**
+     * Criar um Novo Objeto de Formatação de Números
+     *
+     * @param  int             $style Estilo Utilizado no Construtor
+     * @return NumberFormatter Elemento Solicitado
+     */
+    public function createNumberFormatter($style)
+    {
+        // Apresentação
+        return new NumberFormatter($this->getLocale(), $style);
     }
 }
--- module/Balance/test/Balance/I18n/I18nTest.php
+++ module/Balance/test/Balance/I18n/I18nTest.php
@@ -2,6 +2,7 @@
 
 namespace Balance\I18n;
 
+use NumberFormatter;
 use PHPUnit_Framework_TestCase as TestCase;
 
 class I18nTest extends TestCase
@@ -15,5 +16,16 @@ class I18nTest extends TestCase
         $result = $element->setLocale('en_US');
         $this->assertSame($result, $element);
         $this->assertEquals('en_US', $element->getLocale());
+    }
+
+    public function testCreateNumberFormatter()
+    {
+        $element = new I18n('pt_BR');
+
+        $formatter = $element->createNumberFormatter(NumberFormatter::DECIMAL);
+        $this->assertInstanceOf('NumberFormatter', $formatter);
+
+        $anotherFormatter = $element->createNumberFormatter(NumberFormatter::DECIMAL);
+        $this->assertNotSame($formatter, $anotherFormatter);
     }
 }
