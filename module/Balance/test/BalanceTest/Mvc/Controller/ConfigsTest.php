e0bbd380006669c3675d3ccac913db4f6f400d2e
Wanderson <wandersonwhcr@gmail.com>
2015-12-22 17:00:44 -0200

    Issue #120 Testes em controladora

--- module/Balance/test/BalanceTest/Mvc/Controller/ConfigsTest.php
+++ module/Balance/test/BalanceTest/Mvc/Controller/ConfigsTest.php
@@ -2,12 +2,14 @@
 
 namespace BalanceTest\Mvc\Controller;
 
+use Balance\Model\ModelException;
 use Balance\Mvc\Application;
 use Balance\Mvc\Controller\Configs;
 use PHPUnit_Framework_TestCase as TestCase;
 use Zend\Http;
 use Zend\Mvc\Router;
 use Zend\ServiceManager\ServiceManager;
+use Zend\Stdlib\Parameters;
 
 class ConfigsTest extends TestCase
 {
@@ -61,9 +63,66 @@ class ConfigsTest extends TestCase
             'action' => 'modules',
         ]));
         // Execução
-        $result = $element->dispatch($this->getMock('Zend\Stdlib\RequestInterface'));
+        $result = $element->dispatch(new Http\PhpEnvironment\Request());
         // Verificações
         $this->assertInstanceOf('Zend\View\Model\ViewModel', $result);
         $this->assertEquals([], $result->elements);
+    }
+
+    public function testModulesAndSave()
+    {
+        // Inicialização
+        $element = new Configs();
+        // Localizador de Serviços
+        $serviceLocator = new ServiceManager();
+        $element->setServiceLocator($serviceLocator);
+        // Camada de Modelo
+        $mModules = $this->getMock('Balance\Model\Modules');
+        // Dados de Salvamento
+        $data = new Parameters(['modules' => ['ModuleA', 'ModuleB']]);
+        // Salvar com Dados Corretos
+        $mModules->expects($this->once())
+            ->method('save')
+            ->with($this->equalTo($data))
+            ->will($this->returnSelf());
+        // Serviço
+        $serviceLocator->setService('Balance\Model\Modules', $mModules);
+        // Configurar Parâmetros de Despacho
+        $element->getEvent()->setRouteMatch(new Router\RouteMatch([
+            'action' => 'modules',
+        ]));
+        // Requisição
+        $request = (new Http\PhpEnvironment\Request())
+            ->setMethod('POST')
+            ->setPost($data);
+        // Execução
+        $result = $element->dispatch($request);
+        // Verificações
+        $this->assertInstanceOf('Zend\View\Model\ViewModel', $result);
+    }
+
+    public function testModulesAndSaveWithErrors()
+    {
+        // Inicialização
+        $element = new Configs();
+        // Localizador de Serviços
+        $serviceLocator = new ServiceManager();
+        $element->setServiceLocator($serviceLocator);
+        // Camada de Modelo
+        $mModules = $this->getMock('Balance\Model\Modules');
+        // Salvar com Dados Corretos
+        $mModules->expects($this->once())
+            ->method('save')
+            ->will($this->throwException(new ModelException()));
+        // Serviço
+        $serviceLocator->setService('Balance\Model\Modules', $mModules);
+        // Configurar Parâmetros de Despacho
+        $element->getEvent()->setRouteMatch(new Router\RouteMatch([
+            'action' => 'modules',
+        ]));
+        // Requisição
+        $request = (new Http\PhpEnvironment\Request())->setMethod('POST');
+        // Execução
+        $element->dispatch($request);
     }
 }
