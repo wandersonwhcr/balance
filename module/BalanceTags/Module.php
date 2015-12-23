22f4eed2120514258260b13cd1ec2dad1f4488a7
Wanderson <wandersonwhcr@gmail.com>
2015-12-23 14:58:06 -0200

    Issue #71 Módulo do Balance

--- module/BalanceTags/Module.php
+++ module/BalanceTags/Module.php
@@ -2,10 +2,51 @@
 
 namespace BalanceTags;
 
-class Module
+use Balance\Module\ModuleInterface;
+
+/**
+ * Módulo de Tags
+ *
+ * Possibilita o relacionamento de lançamentos a etiquetas previamente cadastradas. Isto facilita algumas pesquisas,
+ * adicionando fatores de pesquisa e agrupamentos.
+ */
+class Module implements ModuleInterface
 {
+    /**
+     * Configurações do Módulo
+     *
+     * @return array Valores Solicitados
+     */
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIdentifier()
+    {
+        return 'BalanceTags';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'Etiquetas';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription()
+    {
+        return <<<DESCRIPTION
+Adiciona a possibilidade de relacionamento de lançamentos com etiquetas previamente cadastradas, facilitando pesquisas e
+agrupamentos de informações. A pesquisa de lançamentos também recebe um filtro de etiquetas, aumentando a possibilidade
+de pesquisas.
+DESCRIPTION;
     }
 }
