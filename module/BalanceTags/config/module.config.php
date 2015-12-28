0b78a0abd4d6f9edc8917b76c5cc7a66df41bb61
Wanderson <wandersonwhcr@gmail.com>
2015-12-28 13:58:36 -0200

    Issue #71 Menu Principal

--- module/BalanceTags/config/module.config.php
+++ module/BalanceTags/config/module.config.php
@@ -1,2 +1,82 @@
 <?php
-return [];
+return [
+    'router' => [
+        'routes' => [
+            'tags' => [
+                'type'    => 'literal',
+                'options' => [
+                    'route'    => '/tags',
+                    'defaults' => [
+                        'controller' => 'BalanceTags\Mvc\Controller\Tags',
+                        'action'     => 'index',
+                    ],
+                ],
+                'may_terminate' => true,
+                'child_routes' => [
+                    'add' => [
+                        'type'    => 'literal',
+                        'options' => [
+                            'route'    => '/add',
+                            'defaults' => [
+                                'action' => 'edit',
+                            ],
+                        ],
+                    ],
+                    'edit' => [
+                        'type'    => 'segment',
+                        'options' => [
+                            'route'    => '/edit/:id',
+                            'defaults' => [
+                                'action' => 'edit',
+                            ],
+                            'constraints' => [
+                                'id' => '[0-9]+',
+                            ],
+                        ],
+                    ],
+                    'remove' => [
+                        'type'    => 'segment',
+                        'options' => [
+                            'route'    => '/remove/:id',
+                            'defaults' => [
+                                'action' => 'remove',
+                            ],
+                            'constraints' => [
+                                'id' => '[0-9]+',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    'navigation' => [
+        'default' => [
+            'balance' => [
+                'pages' => [
+                    'tags' => [
+                        'label' => 'Etiquetas',
+                        'route' => 'tags',
+                        'order' => 150,
+                        'pages' => [
+                            'tags' => [
+                                'label' => 'Listar',
+                                'route' => 'tags',
+                            ],
+                            'add' => [
+                                'label' => 'Adicionar',
+                                'route' => 'tags/add',
+                            ],
+                            'edit' => [
+                                'label'   => 'Editar',
+                                'route'   => 'tags/edit',
+                                'visible' => false,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
