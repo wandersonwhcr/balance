841abf877e2a8cccb8fc4391e66236e3193b2d13
Wanderson <wandersonwhcr@gmail.com>
2015-12-28 14:38:51 -0200

    Issue #71 Estrutura de persistência de etiquetas

--- module/BalanceTags/src/BalanceTags/Model/Persistence/Db/Tags.php
+++ module/BalanceTags/src/BalanceTags/Model/Persistence/Db/Tags.php
@@ -2,8 +2,44 @@
 
 namespace BalanceTags\Model\Persistence\Db;
 
+use ArrayIterator;
 use Balance\Model\Persistence\PersistenceInterface;
+use Zend\Stdlib\Parameters;
 
+/**
+ * Camada de Persistência de Etiquetas
+ */
 class Tags implements PersistenceInterface
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function fetch(Parameters $params)
+    {
+        return new ArrayIterator(array());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function find(Parameters $params)
+    {
+        return array();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function save(Parameters $data)
+    {
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove(Parameters $params)
+    {
+        return $this;
+    }
 }
